9951da8678dfac5b3d9b636711403118a4e2f459
Beto Mossmann <gilbertomossmann@gmail.com>
2023-06-09 10:31:40 -0300

    improve code quality

--- src/pages/components/intro.tsx
+++ src/pages/components/intro.tsx
@@ -1,11 +1,9 @@
 function Intro() {
   return (
     <div className='flex items-center justify-center flex-col text-center pt-20 pb-6'>
-      <h1 className='text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold'>Your Name</h1>
-      <p className='text-base md:text-xl mb-4 font-medium'>Software Engineer</p>
-      <p className='text-sm max-w-xl mb-6 font-bold'>Software Engineer, from ideation to implementation, every coding project I undertake is a labor of love. 
-        As an indie developer, I relish the opportunity to build something from the ground up, and to create 
-        solutions that make a tangible difference in people`s lives.
+      <h1 className='text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold'>Gilberto Mossmann</h1>
+      <p className='text-base md:text-xl mb-4 font-medium'>Full-Stack Developer</p>
+      <p className='text-sm max-w-xl mb-6 font-bold'>I'm an indie full-stack developer and content creator who is dedicated to crafting the digital world through my work. I approach every coding project with a comprehensive methodology that includes planning, designing, and solving real-world issues using cutting-edge technology. I take pride in building each project from scratch, ensuring that every aspect of my work is tailored to meet the specific needs of my clients.
       </p>
     </div>
   )
--- src/pages/data/timeline.ts
+++ src/pages/data/timeline.ts
@@ -10,50 +10,22 @@ export const timelines: Timeline[] = [
   {
     id: 0o1,
     year: 'Today',
-    title: 'Software Engineer',
-    duration: '3 years',
-    details: 'blá blá blá blá blá blá blá blá blá blá'
+    title: 'Solo Indie Developer',
+    duration: '4 years',
+    details: 'As a indie full-stack developer passionate about building innovative digital solutions. With a meticulous methodology and custom coding skills, I craft meaningful tools, experiences, and content for clients from the ground up. My goal is to empower individuals and organizations with imaginative technical solutions that truly resonate.'
   },
   {
     id: 0o2,
-    year: 'Today',
-    title: 'Software Engineer',
-    duration: '3 years',
-    details: 'blá blá blá blá blá blá blá blá blá blá'
+    year: '2020',
+    title: 'bachelor of information technology - Harvard',
+    duration: '5 years',
+    details: 'He received his A.B. summa cum laude in information technology from Harvard College'
   },
   {
     id: 0o3,
-    year: 'Today',
-    title: 'Software Engineer',
-    duration: '3 years',
-    details: 'blá blá blá blá blá blá blá blá blá blá'
-  },
-  {
-    id: 0o4,
-    year: 'Today',
-    title: 'Software Engineer',
-    duration: '3 years',
-    details: 'blá blá blá blá blá blá blá blá blá blá'
-  },
-  {
-    id: 0o5,
-    year: 'Today',
-    title: 'Software Engineer',
-    duration: '3 years',
-    details: 'blá blá blá blá blá blá blá blá blá blá'
-  },
-  {
-    id: 0o6,
-    year: 'Today',
-    title: 'Software Engineer',
-    duration: '3 years',
-    details: 'blá blá blá blá blá blá blá blá blá blá'
-  },
-  {
-    id: 0o7,
-    year: 'Today',
-    title: 'Software Engineer',
-    duration: '3 years',
-    details: 'blá blá blá blá blá blá blá blá blá blá'
+    year: '2011',
+    title: 'First Line of Code',
+    duration: 'The beginning',
+    details: 'I wrote my first "Hello World" started this journey into digital craftsmanship. As someone who had only ever identified as a blue collar worker, I could have never fathomed the doors that would open as a result of this one small act.'
   }
 ]
